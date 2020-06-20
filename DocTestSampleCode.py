a4cecebccbec1cd58742bed895ef70db223b6462
syeddanyalhassan <syeddanyalhassan@gmail.com>
2020-06-20 16:02:44 +0500

    New source file added

---
+++ DocTestSampleCode.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""Spyder Editor
+
+This is a temporary script file."""
+
+"""
+Sample doctest test module
+Execute them on command prompt using python -m doctest -v Filename.py on 
+Anaconda\Lib\test\Testing
+
+"""
+
+def multiply(a, b):
+    """
+    >>> mul(2, 3)
+    6
+    >>> mul('a', 2)
+    'aa'
+    """
+    return a * b
+def add(a,b):
+    """
+    >>> add(2,3)
+    5
+    >>> add('a','b')
+    'ab'
+    """
+    return a+b
+def subtract(a,b):
+    """
+    >>> sub(3,4)
+    1
+    >>> sub(9,8)
+    -1
+    """
+    return b-a
+def divide(a,b):
+    """
+    >>> div(4,8)
+    2.0
+    >>> div(12,12)
+    1.0
+    """
+    return b/a
